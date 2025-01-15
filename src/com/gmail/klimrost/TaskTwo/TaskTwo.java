ff5e3088fc82975906f8701971bf23c624509d1a
klimr <klimrost6@gmail.com>
2025-01-15 22:26:07 +0200

    Sales day

--- src/com/gmail/klimrost/TaskTwo/TaskTwo.java
+++ src/com/gmail/klimrost/TaskTwo/TaskTwo.java
@@ -2,6 +2,31 @@ package com.gmail.klimrost.TaskTwo;
 
 public class TaskTwo {
     public static void main(String[] args) {
+        byte number = 1;
+        String product = "smartphone";
+        double price = 12153.41;
 
+        double total = price / 5;
+
+        System.out.println("Order No " + number);
+        System.out.println("Product: " + product);
+        System.out.printf("Sales by day is EUR: %.2f%n", total);
+        System.out.println("Total sales for 5 days is EUR: " + price);
+
+        number++;
+        product = "laptop";
+        price = 10486.85;
+        total = price / 7;
+        System.out.println("Order No " + number);
+        System.out.println("Product: " + product);
+        System.out.printf("Sales by day is EUR: %.2f%n", total);
+        System.out.println("Total sales for 7 days is EUR: " + price);
     }
 }
+
+
+
+
+
+
+
